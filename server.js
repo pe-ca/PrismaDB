b2606b5ce9f4c7fa3b0ebc0e2bcb65da641c2e71
pe-ca <mr.gloser1@gmail.com>
2022-05-11 12:25:15 -0500

    agregado endpoint que regresa un explorer por id

--- server.js
+++ server.js
@@ -19,3 +19,9 @@ app.get('/explorers', async (req, res) => {
   const allExplorers =  await prisma.explorer.findMany({});
   res.json(allExplorers);
 });
+
+app.get('/explorers/:id', async (req, res) => {
+  const id = req.params.id;
+  const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+  res.json(explorer);
+});
