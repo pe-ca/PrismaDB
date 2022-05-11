3f1c5a2c892620a8240ec04759b9621a9c4c6322
pe-ca <mr.gloser1@gmail.com>
2022-05-11 17:20:17 -0500

    agregados nuevos endpoint para MissionCommander

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
 
+//explorers
 app.get('/explorers', async (req, res) => {
   const allExplorers =  await prisma.explorer.findMany({});
   res.json(allExplorers);
@@ -61,6 +62,7 @@ app.delete('/explorers/:id', async (req, res) => {
   return res.json({message: "Eliminado correctamente"});
 });
 
+//explorers_2
 app.get('/explorers_2', async (req, res) => {
   const allExplorers =  await prisma.Explorer_2.findMany({});
   res.json(allExplorers);
@@ -103,3 +105,47 @@ app.delete('/explorers_2/:id', async (req, res) => {
   await prisma.Explorer_2.delete({where: {id: id}});
   return res.json({message: "Eliminado correctamente"});
 });
+
+//MissionCommander
+app.get('/missioncommander', async(req, res) => {
+  const allMC = await prisma.MissionCommander.findMany({});
+  res.json(allMC);
+});
+
+app.get('/missioncommander/:id', async(req, res) => {
+  const id = req.params.id
+  const MCbyId = await prisma.MissionCommander.findUnique({where: {id: parseInt(id)}});
+  res.json(MCbyId);
+});
+
+app.post('/missioncommander', async(req, res) => {
+  const newMC ={
+    name: req.body.name,
+    username: req.body.username,
+    mainStack: req.body.mainStack,
+    currentEnrollments: req.body.currentEnrollments,
+    hasAzureCertification: req.body.hasAzureCertification,
+  };
+  const message = 'Mission Commander agregado';
+  await prisma.MissionCommander.create({data: newMC});
+  return res.json(message);
+});
+
+app.put('/missioncommander/:id', async(req, res) => {
+  const id = parseInt(req.params.id);
+  await prisma.MissionCommander.update({
+    where:{
+      id: id
+    },
+    data: {
+      mainStack: req.body.mainstack
+    }
+  });
+  return res.json({message: "Actualizado MainStack correctamente"});
+});
+
+app.delete('/missioncommander/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  await prisma.MissionCommander.delete({where: {id:id}});
+  return res.json({message: "Eliminado correctamente"})
+})
